757cf4034539474ea8665b84a1b763c2b53e239e
Shubham Singh <shubh622005@gmail.com>
2024-11-10 17:32:47 +0530

    chore(creds): added domain

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use flux_mail::start_server;
 
 fn main() {
-    let addr: std::net::SocketAddr = "0.0.0.0:2525".parse().unwrap();
+    let addr: std::net::SocketAddr = "0.0.0.0:25".parse().unwrap();
     // ADD CHECK FOR DOMAIN
-    let domain: String = String::from("example.com");
+    let domain: String = String::from("mail.flux.shubh.sh");
     if let Err(e) = start_server(addr, domain) {
         eprintln!("Error starting server: {}", e);
     }
